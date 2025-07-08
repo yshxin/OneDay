7e5d5ee0d983308b240e29d72ad745c77634571b
yushengxin <17760470049@163.com>
2025-07-08 14:38:08 +0800

    修正基础路径为 './' 以匹配项目结构

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/OneDay/', // 仓库名，若不同请改为你的 repo 名
+  base: './', // 仓库名，若不同请改为你的 repo 名
 })
